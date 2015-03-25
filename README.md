636d41b36f182fb24b45f04437127189cbca6382
andberg <andreaberglund@hotmail.com>
2015-03-25 20:03:00 +0100

    lets see if this works

--- README.md
+++
@@ -1,2 +0,0 @@
-# timeline
-A comparison between two Timeline-tools for my last assignment from C3L. 
---
+++ scripts/scripts.eda34f4d.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("timelineApp",["ngCookies","ngResource","ngRoute","ngSanitize","ngTouch"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("timelineApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
